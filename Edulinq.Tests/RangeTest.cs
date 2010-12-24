08afcdba96a3a1f18dc60b26c5b9aad167f145af
Jon Skeet <skeet@pobox.com>
2010-12-24 08:32:19 +0000

    Added Range implementation.

---
+++ Edulinq.Tests/RangeTest.cs
@@ -0,0 +1,57 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
+// This time we can't just use Enumerable, as that will always use Edulinq.Enumerable
+//using RangeClass = System.Linq.Enumerable;
+using RangeClass = Edulinq.Enumerable;
+using NUnit.Framework;
+
+namespace Edulinq.Tests
+{
+    [TestFixture]
+    public class RangeTest
+    {
+        [Test]
+        public void NegativeCount()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => RangeClass.Range(10, -1));
+        }
+
+        [Test]
+        public void CountTooLarge()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => RangeClass.Range(int.MaxValue, 2));
+            Assert.Throws<ArgumentOutOfRangeException>(() => RangeClass.Range(2, int.MaxValue));
+            // int.MaxValue is odd, hence the +3 instead of +2
+            Assert.Throws<ArgumentOutOfRangeException>(() => RangeClass.Range(int.MaxValue / 2, (int.MaxValue / 2) + 3));
+        }
+
+        [Test]
+        public void LargeButValidCount()
+        {
+            // Essentially the edge conditions for CountTooLarge, but just below the boundary
+            RangeClass.Range(int.MaxValue, 1);
+            RangeClass.Range(1, int.MaxValue);
+            RangeClass.Range(int.MaxValue / 2, (int.MaxValue / 2) + 2);
+        }
+
+        [Test]
+        public void ValidRange()
+        {
+            RangeClass.Range(5, 3).AssertSequenceEqual(5, 6, 7);
+        }
+    }
+}
---
+++ Edulinq/Range.cs
@@ -0,0 +1,47 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Edulinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<int> Range(int start, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException("count");
+            }
+            // Convert everything to long to avoid overflows. There are other ways of checking
+            // for overflow, but this way make the code correct in the most obvious way.
+            if ((long)start + (long)count - 1L > int.MaxValue)
+            {
+                throw new ArgumentOutOfRangeException("count");
+            }
+            return RangeImpl(start, count);
+        }
+
+        private static IEnumerable<int> RangeImpl(int start, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return start + i;
+            }
+        }
+    }
+}
