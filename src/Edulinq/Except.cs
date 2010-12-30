4e4d81ca8f617073d47ae04c0361aa7257f042fe
Jon Skeet <skeet@pobox.com>
2010-12-30 21:44:51 +0000

    Implemented Except.

---
+++ src/Edulinq/Except.cs
@@ -0,0 +1,61 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
+using System.Collections.Generic;
+
+namespace Edulinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TSource> Except<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second)
+        {
+            return Except(first, second, EqualityComparer<TSource>.Default);
+        }
+
+        public static IEnumerable<TSource> Except<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {           
+            if (first == null)
+            {
+                throw new ArgumentNullException("first");
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException("second");
+            }
+            return ExceptImpl(first, second, comparer ?? EqualityComparer<TSource>.Default);
+        }
+
+        private static IEnumerable<TSource> ExceptImpl<TSource>(
+            IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {
+            HashSet<TSource> bannedElements = new HashSet<TSource>(second, comparer);
+            foreach (TSource item in first)
+            {
+                if (bannedElements.Add(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+    }
+}
