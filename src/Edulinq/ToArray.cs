52d7525dd043d1f016f3543e00e7cc2f335cd715
Jon Skeet <skeet@pobox.com>
2011-01-02 19:54:01 +0000

    Implemented ToArray

---
+++ src/Edulinq/ToArray.cs
@@ -0,0 +1,40 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using System.Collections.Generic;
+
+namespace Edulinq
+{
+    public static partial class Enumerable
+    {
+        public static TSource[] ToArray<TSource>(this IEnumerable<TSource> source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException("source");
+            }
+            ICollection<TSource> collection = source as ICollection<TSource>;
+            if (collection != null)
+            {
+                TSource[] ret = new TSource[collection.Count];
+                collection.CopyTo(ret, 0);
+                return ret;
+            }
+            return new List<TSource>(source).ToArray();
+        }
+    }
+}
