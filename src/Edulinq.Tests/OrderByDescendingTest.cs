4aa8763c161902417b3648441e4dd3a231864746
Jon Skeet <skeet@pobox.com>
2011-01-04 20:27:33 +0000

    Preparation for OrderBy etc.

---
+++ src/Edulinq.Tests/OrderByDescendingTest.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using Edulinq.TestSupport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Edulinq.Tests
+{
+    [TestFixture]
+    public class OrderByDescendingTest
+    {
+        [Test]
+        public void SimpleUniqueKeys()
+        {
+            var source = new[]
+            {
+                new { Value = 1, Key = "abc" },
+                new { Value = 2, Key = "ghi" },
+                new { Value = 3, Key = "def" }
+            };
+            var query = source.OrderByDescending(x => x.Key)
+                              .Select(x => x.Value);
+            query.AssertSequenceEqual(2, 3, 1);
+        }
+
+        [Test]
+        public void NullsAreLast()
+        {
+            var source = new[]
+            {
+                new { Value = 1, Key = "abc" },
+                new { Value = 2, Key = (string) null },
+                new { Value = 3, Key = "def" }
+            };
+            var query = source.OrderByDescending(x => x.Key)
+                              .Select(x => x.Value);
+            query.AssertSequenceEqual(3, 1, 2);
+        }
+
+        [Test]
+        public void OrderingIsStable()
+        {
+            var source = new[]
+            {
+                new { Value = 1, Key = "abc" },
+                new { Value = 2, Key = "def" },
+                new { Value = 3, Key = "def" },
+                new { Value = 4, Key = "abc" },
+            };
+            var query = source.OrderByDescending(x => x.Key)
+                              .Select(x => x.Value);
+            query.AssertSequenceEqual(2, 3, 1, 4);
+        }
+    }
+}
---
+++ src/Edulinq/OrderedEnumerable.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+
+namespace Edulinq
+{
+    internal class OrderedEnumerable<TElement> : IOrderedEnumerable<TElement>
+    {
+        private readonly IEnumerable<TElement> source;
+        private readonly IComparer<TElement> comparer;
+
+        internal OrderedEnumerable(IEnumerable<TElement> source,
+            IComparer<TElement> comparer)
+        {
+            this.source = source;
+            this.comparer = comparer;
+        }
+
+        public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        {
+            IComparer<TElement> secondaryComparer = new ProjectionComparer<TElement, TKey> (keySelector, comparer);
+            if (descending)
+            {
+                secondaryComparer = new ReverseComparer<TElement>(secondaryComparer);
+            }
+            return new OrderedEnumerable<TElement>(source,
+                new CompoundComparer<TElement>(this.comparer, secondaryComparer));
+        }
+
+        public IEnumerator<TElement> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
