900d6d2c9467a643f608b0c033ee8adc8a14da65
Jon Skeet <skeet@pobox.com>
2011-01-04 20:50:41 +0000

    - Implemented extension methods - Implemented OrderedEnumerable.GetEnumerator in hacky way

---
+++ src/Edulinq/OrderByDescending.cs
@@ -0,0 +1,48 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
+using System.Collections.Generic;
+
+namespace Edulinq
+{
+    public static partial class Enumerable
+    {
+        public static IOrderedEnumerable<TSource> OrderByDescending<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector)
+        {
+            return OrderBy(source, keySelector, Comparer<TKey>.Default);
+        }
+
+        public static IOrderedEnumerable<TSource> OrderByDescending<TSource, TKey>(
+            this IEnumerable<TSource> source,
+            Func<TSource, TKey> keySelector,
+            IComparer<TKey> comparer)
+        {
+            if (source == null)
+            {
+                throw new ArgumentException("source");
+            }
+            if (keySelector == null)
+            {
+                throw new ArgumentException("keySelector");
+            }
+            IComparer<TSource> sourceComparer = new ProjectionComparer<TSource, TKey>(keySelector, comparer);
+            sourceComparer = new ReverseComparer<TSource>(sourceComparer);
+            return new OrderedEnumerable<TSource>(source, sourceComparer);
+        }
+    }
+}
--- src/Edulinq/OrderedEnumerable.cs
+++ src/Edulinq/OrderedEnumerable.cs
@@ -1,37 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Edulinq
 {
     internal class OrderedEnumerable<TElement> : IOrderedEnumerable<TElement>
     {
         private readonly IEnumerable<TElement> source;
-        private readonly IComparer<TElement> comparer;
+        private readonly IComparer<TElement> currentComparer;
 
         internal OrderedEnumerable(IEnumerable<TElement> source,
             IComparer<TElement> comparer)
         {
             this.source = source;
-            this.comparer = comparer;
+            this.currentComparer = comparer;
         }
 
         public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
         {
+            if (keySelector == null)
+            {
+                throw new ArgumentNullException("keySelector");
+            }
             IComparer<TElement> secondaryComparer = new ProjectionComparer<TElement, TKey> (keySelector, comparer);
             if (descending)
             {
                 secondaryComparer = new ReverseComparer<TElement>(secondaryComparer);
             }
             return new OrderedEnumerable<TElement>(source,
-                new CompoundComparer<TElement>(this.comparer, secondaryComparer));
+                new CompoundComparer<TElement>(currentComparer, secondaryComparer));
         }
 
         public IEnumerator<TElement> GetEnumerator()
         {
-            throw new NotImplementedException();
+            // This is a truly sucky way of implementing it. It's the simplest I could think of to start with.
+            // We'll come back to it!
+            List<TElement> elements = source.ToList();
+            while (elements.Count > 0)
+            {
+                TElement minElement = elements[0];
+                int minIndex = 0;
+                for (int i = 1; i < elements.Count; i++)
+                {
+                    if (currentComparer.Compare(minElement, elements[i]) < 0)
+                    {
+                        minElement = elements[i];
+                        minIndex = i;
+                    }
+                }
+                elements.RemoveAt(minIndex);
+                yield return minElement;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
