fd72e1f598fc93cf71270cc8d83514ca686f0ef8
Jon Skeet <skeet@pobox.com>
2011-01-12 19:44:07 +0000

    Implemented Contains.

---
+++ src/Edulinq.TestSupport/HashSetProvider.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edulinq.TestSupport
+{
+    public static class HashSetProvider
+    {
+        /// <summary>
+        /// Allows a HashSet to be created in test classes, even though the Edulinq
+        /// configuration of the tests project doesn't have a reference to System.Core.
+        /// Basically it's a grotty hack.
+        /// </summary>
+        public static ICollection<T> NewHashSet<T>(IEqualityComparer<T> comparer, params T[] items)
+        {
+            return new HashSet<T>(items, comparer);
+        }
+    }
+}
