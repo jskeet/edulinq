a33df0128d96d38831e3f405fa2a502757cb2428
Jon Skeet <skeet@pobox.com>
2011-01-12 19:41:37 +0000

    Refactored to use single TryElementAt method.

--- src/Edulinq/ElementAtOrDefault.cs
+++ src/Edulinq/ElementAtOrDefault.cs
@@ -13,9 +13,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-using System;
 using System.Collections.Generic;
-using System.Collections;
 
 namespace Edulinq
 {
@@ -25,57 +23,10 @@ namespace Edulinq
             this IEnumerable<TSource> source,
             int index)
         {
-            if (source == null)
-            {
-                throw new ArgumentNullException("source");
-            }
-            if (index < 0)
-            {
-                return default(TSource);
-            }
-            ICollection<TSource> collection = source as ICollection<TSource>;
-            if (collection != null)
-            {
-                int count = collection.Count;
-                if (index >= count)
-                {
-                    return default(TSource);
-                }
-                // If it's a list, we know we're okay now - just return directly...
-                IList<TSource> list = source as IList<TSource>;
-                if (list != null)
-                {
-                    return list[index];
-                }
-                // Okay, non-list collection: we'll have to iterate, but at least
-                // we've caught any invalid index values early.
-            }
-
-            // For non-generic collections all we can do is an early bounds check.
-            ICollection nonGenericCollection = source as ICollection;
-            if (nonGenericCollection != null)
-            {
-                int count = nonGenericCollection.Count;
-                if (index >= count)
-                {
-                    return default(TSource);
-                }
-            }
-            // We don't need to fetch the current value each time - get to the right
-            // place first.
-            using (IEnumerator<TSource> iterator = source.GetEnumerator())
-            {
-                // Note use of -1 so that we start off my moving onto element 0.
-                // Don't want to use i <= index in case index==int.MaxValue!
-                for (int i = -1; i < index; i++)
-                {
-                    if (!iterator.MoveNext())
-                    {
-                        return default(TSource);
-                    }
-                }
-                return iterator.Current;
-            }
+            TSource ret;
+            // We don't care about the return value - ret will be default(TSource) if it's false
+            TryElementAt(source, index, out ret);
+            return ret;
         }
     }
 }
