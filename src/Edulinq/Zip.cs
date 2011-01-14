22aad63f96ed5cbe32a1780f2d3a0e3b2580d854
Jon Skeet <skeet@pobox.com>
2011-01-14 21:09:26 +0000

    Implemented Zip.

---
+++ src/Edulinq/Zip.cs
@@ -0,0 +1,60 @@
+﻿#region Copyright and license information
+// Copyright 2010-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Edulinq
+{
+    public static partial class Enumerable
+    {
+        public static IEnumerable<TResult> Zip<TFirst, TSecond, TResult>(
+            this IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second,
+            Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            if (first == null)
+            {
+                throw new ArgumentNullException("first");
+            }
+            if (second == null)
+            {
+                throw new ArgumentNullException("second");
+            }
+            if (resultSelector == null)
+            {
+                throw new ArgumentNullException("resultSelector");
+            }
+            return ZipImpl(first, second, resultSelector);
+        }
+
+        private static IEnumerable<TResult> ZipImpl<TFirst, TSecond, TResult>(
+            IEnumerable<TFirst> first,
+            IEnumerable<TSecond> second,
+            Func<TFirst, TSecond, TResult> resultSelector)
+        {
+            using (IEnumerator<TFirst> iterator1 = first.GetEnumerator())
+            using (IEnumerator<TSecond> iterator2 = second.GetEnumerator())
+            {
+                while (iterator1.MoveNext() && iterator2.MoveNext())
+                {
+                    yield return resultSelector(iterator1.Current, iterator2.Current);
+                }
+            }
+        }
+    }
+}
